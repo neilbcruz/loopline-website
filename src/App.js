f79428798545360de236e39abfbb7e9207f05ba0
Neil Cruz <neilbcruz@gmail.com>
2024-01-22 10:29:13 -0500

    remove bootstrap file

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />
-            
+
             <Route path='/inspiration' element={<InspirationPage />} />
             <Route path='/inspiration/australia' element={<VacayAustralia />} />
             <Route path='/inspiration/italy' element={<VacayItaly />} />
